72448355b45c6b4bf5d511eef862eaafd604d829
Daniel Chapman <Scriptor.Deo@gmail.com>
2020-05-21 18:54:07 -0400

    Styling changes

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -19,12 +19,16 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "monospace" :size 14))
+(setq doom-font (font-spec :family "Bookman Old Style" :size 14))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-nova)
+(setq doom-theme 'doom-solarized-light)
+
+
+;; Hide Line numbers
+(setq display-line-numbers-type nil)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
