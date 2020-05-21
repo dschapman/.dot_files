7306fd06636c34527c7f5988b1ed952bad5b6b28
Daniel Chapman <Scriptor.Deo@gmail.com>
2020-05-21 06:36:17 -0400

    Updated config to allow moving sections to other files loaded in the buffer

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -79,3 +79,23 @@
       (deft-use-filter-string-for-filename t)
       (deft-default-extension "org")
       (deft-directory org_notes))
+
+;; Allows you to refile into different files - specifically to
+;; create new 'parent' headings
+(defun +org/opened-buffer-files ()
+  "Return the list of files currently opened in emacs"
+  (delq nil
+        (mapcar (lambda (x)
+                  (if (and (buffer-file-name x)
+                           (string-match "\\.org$"
+                                         (buffer-file-name x)))
+                      (buffer-file-name x)))
+                (buffer-list))))
+
+(setq org-refile-targets '((+org/opened-buffer-files :maxlevel . 9)))
+
+(setq org-refile-use-outline-path 'file)
+;; makes org-refile outline working with helm/ivy
+(setq org-outline-path-complete-in-steps nil)
+(setq org-refile-allow-creating-parent-nodes 'confirm)
+;;
